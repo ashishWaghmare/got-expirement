ad6117fb87cd7b64e1b2287bf783fefdea07b06b
Ashish Waghmare <ashish.waghmare@stepsolutions.com>
2015-02-11 23:02:55 +0530

    http hello world

---
+++ httpsrv.go
@@ -0,0 +1,14 @@
+package main
+
+import "io"
+import "net/http"
+
+func hello(w http.ResponseWriter,r *http.Request) {
+	io.WriteString(w,"Hello World")
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+	
+}
